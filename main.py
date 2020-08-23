398d5655b3e675eba7c1291c6805423c2c13bc47
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-08-23 11:46:34 -0400

    cleaned up code

--- main.py
+++ main.py
@@ -68,30 +68,35 @@ while(cap.isOpened() and cap2.isOpened()):
                                                 corners[eindex][0][2],
                                                 corners[eindex][0][3]]), width, height)
             cv2.imshow('warp', warp)
-        
+            
+            # compute the inverse
             M_inverse = np.linalg.inv(M)
-            #back_warp = cv2.warpPerspective(speed_sign, M_inverse, (frame_width, frame_height))
             back_warp = cv2.warpPerspective(video_frame, M_inverse, (frame_width, frame_height))
         
             # Load two images
             img2 = back_warp
-            # I want to put logo on top-left corner, So I create a ROI
-            rows,cols,channels = img2.shape
+            # create an ROI representing the top left corner
+            rows, cols, channels = img2.shape
             roi = img1[0:rows, 0:cols]
-            # Now create a mask of logo and create its inverse mask also
+            
+            # create a mask of the logo
             img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
             ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+            # inverse mask
             mask_inv = cv2.bitwise_not(mask)
-            # Now black-out the area of logo in ROI
+            
+            # black-out the area of logo in ROI
             img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+            
             # Take only region of logo from logo image.
             img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+            
             # Put logo in ROI and modify the main image
             dst2 = cv2.add(img1_bg,img2_fg)
             img1[0:rows, 0:cols ] = dst2
 
     cv2.imshow('augmented', img1)
-    #cv2.imshow('frame_markers', frame_markersarkers)
+    cv2.imshow('frame_markers', frame_markers)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
