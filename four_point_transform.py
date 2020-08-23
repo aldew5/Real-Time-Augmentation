9f4e938325997564fc38bb61b88d48819b4c37aa
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-08-23 12:37:27 -0400

    add augment function

--- four_point_transform.py
+++ four_point_transform.py
@@ -43,4 +43,41 @@ def four_point_transform(image, points, width=0, height=0):
         warped = cv2.warpPerspective(image, M, (width, height))
     
     return (warped, M)
+
+def augment(eindex, frame, corners, image_dim, video_frame, frame_dim, image):
+    width, height = image_dim[0], image_dim[1]
+    frame_width, frame_height = frame_dim[0], frame_dim[1]
     
+    (warp, M) = four_point_transform(frame, np.array([corners[eindex][0][0],
+                                                corners[eindex][0][1],
+                                                corners[eindex][0][2],
+                                            corners[eindex][0][3]]), width, height)
+    cv2.imshow('warp', warp)
+    
+    # compute the inverse
+    M_inverse = np.linalg.inv(M)
+    back_warp = cv2.warpPerspective(video_frame, M_inverse, (frame_width, frame_height))
+
+    # Load two images
+    img2 = back_warp
+    # create an ROI representing the top left corner
+    rows, cols, channels = img2.shape
+    roi = image[0:rows, 0:cols]
+    
+    # create a mask of the logo
+    img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+    # inverse mask
+    mask_inv = cv2.bitwise_not(mask)
+    
+    # black-out the area of logo in ROI
+    image_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+    
+    # Take only region of logo from logo image.
+    img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+    
+    # Put logo in ROI and modify the main image
+    dst2 = cv2.add(image_bg,img2_fg)
+    image[0:rows, 0:cols ] = dst2
+    
+
